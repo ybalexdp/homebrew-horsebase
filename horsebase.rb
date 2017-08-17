672d4e2f6378c654db9f2fcf6e42e540eecea1b8
ybalexdp <ybalexdp@gmail.com>
2017-08-17 18:17:11 +0900

    Release v0.2.0

--- horsebase.rb
+++ horsebase.rb
@@ -6,8 +6,8 @@ class Horsebase < Formula
   desc "Stores horse race data in the DB."
   homepage "https://github.com/ybalexdp/horsebase"
   url "https://github.com/ybalexdp/horsebase/releases/download/v0.2.0/horsebase_darwin_amd64.zip"
-  sha256 "188546c142f337d26e7032a7d583b04395ee2e73e70f0f5064aa38e3c766e59c"
-
+  #sha256 "188546c142f337d26e7032a7d583b04395ee2e73e70f0f5064aa38e3c766e59c"
+  sha256 "6234070fe5c9957b699b66b870734a390b3f8ef4892f793106e262bc5b0860a6"
   # depends_on "cmake" => :build
 
   def install
